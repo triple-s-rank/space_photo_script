50999b2a21cd37495ae7ffd9d68cd2027e9f9f10
triple-s-rank <specialforwar@icloud.com>
2022-07-03 23:58:15 +0300

    Download hubble jpeg

---
+++ space_script.py
@@ -0,0 +1,9 @@
+import requests
+
+filename = "hubble.jpeg"
+url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
