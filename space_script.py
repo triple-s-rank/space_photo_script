ec9cfc63431056107b934f22bd367bca6e8f3238
triple-s-rank <specialforwar@icloud.com>
2022-07-04 00:33:05 +0300

    Add safe creation directory images and downloading image in it

--- space_script.py
+++ space_script.py
@@ -1,9 +1,12 @@
 import requests
+from pathlib import Path
+
+Path(f'{Path.cwd()}\images').mkdir(parents=True, exist_ok=True)
 
 filename = "hubble.jpeg"
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(f'images/{filename}', 'wb') as file:
     file.write(response.content)
