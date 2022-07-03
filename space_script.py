0b25966cb74c7698ec95084168586d16abd5d579
triple-s-rank <specialforwar@icloud.com>
2022-07-04 00:37:38 +0300

    Create download_image function with image url and download path params

--- space_script.py
+++ space_script.py
@@ -3,10 +3,16 @@ from pathlib import Path
 
 Path(f'{Path.cwd()}\images').mkdir(parents=True, exist_ok=True)
 
+
+def download_image(image_url, filename):
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
+
+
 filename = "hubble.jpeg"
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-response = requests.get(url)
-response.raise_for_status()
 
-with open(f'images/{filename}', 'wb') as file:
-    file.write(response.content)
+download_image(url, filename)
