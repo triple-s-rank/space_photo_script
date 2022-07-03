644fd5060e0658a108bc1e7d5826a282c032bf27
triple-s-rank <specialforwar@icloud.com>
2022-07-04 02:08:06 +0300

    Define fetch_spacex_last_launch function

--- space_script.py
+++ space_script.py
@@ -14,5 +14,16 @@ def download_image(image_url, filename):
 
 filename = "hubble.jpeg"
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-
 download_image(url, filename)
+
+
+def fetch_spacex_last_launch(launch_id):
+    launch_url = f'https://api.spacexdata.com/v3/launches/{launch_id}'
+    response = requests.get(url=launch_url)
+    response.raise_for_status()
+    launch_images = response.json()['links']['flickr_images']
+    for image_number, image_url in enumerate(launch_images, start=0):
+        download_image(image_url, f'spacex_{image_number}.jpeg')
+
+
+fetch_spacex_last_launch(67)
